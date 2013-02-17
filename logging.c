19712f124058c4c49e65b264e4adcd5aa6d287ec
Holger Langenau <holger.langenau@s2004.tu-chemnitz.de>
2013-02-17 10:47:32 +0100

    Output timestamp in log.

--- logging.c
+++ logging.c
@@ -13,10 +13,29 @@ void log_set_verbose(gboolean verbose) {
 
 void log_log(gchar * fmt, ...) {
   va_list args;
+  FILE *f;
   
   if (_log_verbose) {
-    va_start(args, fmt);
-    vprintf(fmt, args);
-    va_end(args);
+    time_t t;
+    static char buf[64];
+    static struct tm bdt;
+
+    time(&t);
+    localtime_r(&t, &bdt);
+    strftime(buf, 63, "[%Y%m%d-%H%M%S] ", &bdt);
+    f = fopen("/var/log/fritz2ci.log", "at");
+    if (f != NULL) {
+      fputs(buf, f);
+      va_start(args, fmt);
+      vfprintf(f, fmt, args);
+      va_end(args);
+      fclose(f);
+    }
+    else {
+      fputs(buf, stdin);
+      va_start(args, fmt);
+      vprintf(fmt, args);
+      va_end(args);
+    }
   }
 }
